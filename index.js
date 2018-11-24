96e4deb2f7659e7f7c6acfb7838e1d05f690ee09
Malaj Alexandre <alexandre.malaj@gmail.com>
2018-11-24 07:36:53 +0100

    chore: improve storm rules

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const is = require("@slimio/is");
 const Alerting = new Addon("Alerting");
 
 const stormRules = new Map([
-    ["2#first_test", { time: 60, occurence: 10, severity: 4 }]
+    ["2#first_test", { time: 1, occurence: 10, severity: 4, timestamps: [] }]
 ]);
 
-const thresholds = new Map([
-    [3, 200000]
-]);
+// const thresholds = new Map([
+//     [3, 200000]
+// ]);
 
 /**
  * @async
@@ -35,30 +35,44 @@ async function setStormRule(CID, [time, occurence, severity]) {
     stormRules.set(CID, { time, occurence, severity });
 }
 
-// Alerting.of(Addon.Subjects.Alarm.Open).subscribe((CID) => {
-//     console.log(`[Alerting] Open CID : ${CID}`);
-// });
-
-Alerting.of("Metric.insert").subscribe(([id, value]) => {
-    console.log(`[Alerting] Metric id : ${id}, value: ${value}`);
-
-
+Alerting.of(Addon.Subjects.Alarm.Open).subscribe({
+    next(CID) {
+        console.log(`[Alerting] Open CID : ${CID}`);
+    },
+    error(err) {
+        console.log(`[ALERTING] Alarm.open | Finished with error: ${err}`);
+    }
 });
 
-Alerting.of("Alarm.update").subscribe(([CID, occurence]) => {
-    console.log(`[ALERTING] Updated CID : ${CID}`);
-    console.log(`[ALERTING] Updated occurence : ${occurence}`);
-    if (stormRules.has(CID)) {
-        const data = stormRules.get(CID);
-        Alerting.sendMessage(
-            "events.get_alarms_occurence",
-            { args: [CID, { time: data.time, severity: data.severity }] }
-        ).subscribe((occurence) => {
-            console.log(`[ALERTING] occurence : ${occurence}`);
-            if (occurence >= data.occurence) {
-                console.log("[ALERTING] create storm alarm");
+// Alerting.of("Metric.insert").subscribe(([id, value]) => {
+//     console.log(`[Alerting] Metric id : ${id}, value: ${value}`);
+
+
+// });
+
+Alerting.of("Alarm.update").subscribe({
+    next([CID, occurence, timestamp]) {
+        console.log(`[ALERTING] Updated CID : ${CID}`);
+        console.log(`[ALERTING] Updated occurence : ${occurence}`);
+        console.log(`[ALERTING] Updated timestamp : ${timestamp}`);
+        if (stormRules.has(CID)) {
+            const data = stormRules.get(CID);
+            const limitDate = timestamp - data.time * 60;
+            console.log(`[ALERTING] limitDate : ${limitDate}`);
+            data.timestamps = data.timestamps.filter((dataTime) => dataTime > limitDate);
+
+            data.timestamps.push(timestamp);
+            console.log(data.timestamps);
+            console.log(data.timestamps.length);
+            if (data.timestamps.length >= data.occurence) {
+                console.log(`[ALERTING] CREATE STORM ALARM pccurence: ${data.timestamps.length}`);
+                data.timestamps = [];
             }
-        });
+            stormRules.set(CID, data);
+        }
+    },
+    error(err) {
+        console.log(`[ALERTING] Alarm.update | Finished with error: ${err}`);
     }
 });
 
