6490df06a22778d619fcc5abf8c6667c8a07b6c3
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-04-09 18:53:44 +0200

    chore: work on storm rules

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 // Require Third-Party Dependencies
 const Addon = require("@slimio/addon");
-const is = require("@slimio/is");
+const alert = require("@slimio/alert");
 
-// Globals
-const stormRules = new Map();
+// Require Internal Dependencies
+const StormRule = require("./src/StormRule");
+
+/** @type {Map<String, StormRule>} */
+const Storms = new Map();
 
 // Create Addon
-const Alerting = new Addon("Alerting").lockOn("events");
+const Alerting = new Addon("alerting").lockOn("events");
+
+// Get Alarm Object
+const { Alarm } = alert(Alerting);
 
 /**
  * @async
@@ -18,50 +24,34 @@ const Alerting = new Addon("Alerting").lockOn("events");
  * @param {!Number} rule.occurence Rule occurence
  * @param {!Number} rule.severity Rule severity
  * @returns {Promise<void>}
+ *
+ * @throws {TypeError}
  */
-async function setStormRule(header, CID, { time, occurence, severity }) {
-    if (!is.string(CID)) {
+async function setStormRule(header, CID, rule) {
+    if (typeof CID !== "string") {
         throw new TypeError("CID param must be a type of <string>");
     }
-    if (!is.number(time)) {
-        throw new TypeError("time param must be a type of <number>");
-    }
-    if (!is.number(occurence)) {
-        throw new TypeError("occurence param must be a type of <number>");
-    }
 
-    console.log("[ALERTING] Set new storm Rule");
-    stormRules.set(CID, { time, occurence, severity, timestamps: [] });
+    const { time = 60, occurence, severity = Alarm.Severity.Minor } = rule;
+    Storms.set(CID, new StormRule(time, occurence, severity));
 }
 
-Alerting.of(Addon.Subjects.Alarm.Update).subscribe({
-    next([CID, occurence, timestamp]) {
-        console.log(`[ALERTING] Updated CID : ${CID}`);
-        console.log(`[ALERTING] Updated occurence : ${occurence}`);
-        console.log(`[ALERTING] Updated timestamp : ${timestamp}`);
-        if (stormRules.has(CID)) {
-            const data = stormRules.get(CID);
-            const limitDate = (timestamp - data.time) * 60;
-            console.log(`[ALERTING] limitDate : ${limitDate}`);
-            data.timestamps = data.timestamps.filter((dataTime) => dataTime > limitDate);
-
-            data.timestamps.push(timestamp);
-            console.log(data.timestamps);
-            console.log(data.timestamps.length);
-            if (data.timestamps.length >= data.occurence) {
-                console.log(`[ALERTING] CREATE STORM ALARM pccurence: ${data.timestamps.length}`);
-                data.timestamps = [];
-            }
-            stormRules.set(CID, data);
+// Catch Alarm Update events
+Alerting.of(Addon.Subjects.Alarm.Update).filter(([CID]) => Storms.has(CID)).subscribe({
+    next([CID]) {
+        const rule = Storms.get(CID);
+        if (!rule.walk()) {
+            return;
         }
+
+        new Alarm(`Storm threshold below ${rule.occurence}`, {
+            correlateKey: `storm_${CID}`,
+            severity: rule.severity
+        });
     },
     error(err) {
         console.log(`[ALERTING] Alarm.update | Finished with error: ${err}`);
     }
-});
-
-Alerting.on("start", () => {
-    console.log("[ALERTING] Start event triggered !");
 });
 
 Alerting.on("awake", () => {
---
+++ src/StormRule.js
@@ -0,0 +1,56 @@
+// Require Third-party Dependencies
+const TimeMap = require("@slimio/timemap");
+
+// Symbols
+const symRows = Symbol("rows");
+
+/**
+ * @class StormRule
+ */
+class StormRule {
+    /**
+     * @constructor
+     * @memberof StormRule#
+     * @param {!Number} time time delay in milliseconds
+     * @param {!Number} occurence maximum number of occurence in the time
+     * @param {!Number} severity severity
+     *
+     * @throws {TypeError}
+     */
+    constructor(time, occurence, severity) {
+        if (typeof time !== "number") {
+            throw new TypeError("time must be a number");
+        }
+        if (typeof occurence !== "number") {
+            throw new TypeError("occurence must be a number");
+        }
+        if (typeof severity !== "number") {
+            throw new TypeError("severity must be a number");
+        }
+
+        this.occurence = occurence;
+        this.severity = severity;
+        Reflect.defineProperty(this, symRows, { value: new TimeMap(time * 1000) });
+    }
+
+    /**
+     * @method walk
+     * @memberof StormRule#
+     * @return {Boolean}
+     */
+    walk() {
+        const rows = this[symRows];
+        rows.set(Date.now(), null);
+
+        console.log(rows.size);
+        if (rows.size >= this.occurence) {
+            rows.clear();
+
+            return true;
+        }
+
+        return false;
+    }
+}
+
+module.exports = StormRule;
