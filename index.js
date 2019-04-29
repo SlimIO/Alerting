ab377e443f8c325d2c2d2262ab85a8d5c91f29f4
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-04-29 23:32:19 +0200

    chore: implement first prototype of entities assertion

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 // Require Third-Party Dependencies
 const Addon = require("@slimio/addon");
+const timer = require("@slimio/timer");
 const alert = require("@slimio/alert");
 
 // Require Internal Dependencies
 const StormRule = require("./src/StormRule");
 
+// CONSTANTS
+const ENTITY_INTERVAL_MS = 5000;
+
 /** @type {Map<String, StormRule>} */
 const Storms = new Map();
+
+/** @type {Map<String, any>} */
+const Entities = new Map();
+
+let entityInterval;
 
 // Create Addon
 const Alerting = new Addon("alerting").lockOn("events");
 
 // Get Alarm Object
 const { Alarm } = alert(Alerting);
+
+function sendMessage(target, args) {
+    return new Promise((resolve, reject) => {
+        Alerting.sendMessage(target, { args }).subscribe(resolve, reject);
+    });
+}
 
 /**
  * @async
@@ -53,7 +68,51 @@ async function assertEntity(header, entityName, options) {
         throw new TypeError("entityName param must be typeof <string>");
     }
 
+    console.log("RECEIVING ASSERT ENTITY");
     const { exist = true, parent = null } = options;
+    if (parent !== null) {
+        const pType = typeof parent;
+        if (pType !== "number" || pType !== "string") {
+            throw new TypeError("parent param must be typeof <number> or <string>");
+        }
+
+        let entity;
+        if (pType === "number") {
+            entity = await sendMessage("events.get_entity_by_id", [parent]);
+        }
+        else {
+            entity = await sendMessage("events.search_entities", [{ name: parent }]);
+        }
+
+        // TODO: verify if parent exist!
+    }
+    Entities.set(entityName, { exist, parent });
+}
+
+async function assertEntityInterval() {
+    console.log("assert entity interval");
+    for (const [entity, options] of Entities.entries()) {
+        try {
+            const ret = await sendMessage("events.search_entities", [{ name: entity }]);
+            const isDefined = typeof ret !== "undefined";
+            console.log("Entity: \n", ret);
+
+            const correlateKey = `alert_ae_${entity.toLowerCase()}`;
+            if (options.exist && !isDefined) {
+                new Alarm(`Unable to found entity with name ${entity}`, {
+                    correlateKey
+                });
+            }
+            else if (!options.exist && isDefined) {
+                new Alarm(`Entity '${entity}' has been detected but it should not exist (Alerting Assertion).`, {
+                    entity, correlateKey
+                });
+            }
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
 }
 
 // Catch Alarm Update events
@@ -75,7 +134,12 @@ Alerting.of(Addon.Subjects.Alarm.Update).filter(([CID]) => Storms.has(CID)).subs
 });
 
 Alerting.on("awake", () => {
+    entityInterval = timer.setInterval(assertEntityInterval, ENTITY_INTERVAL_MS);
     Alerting.ready();
+});
+
+Alerting.on("close", () => {
+    timer.clearInterval(entityInterval);
 });
 
 Alerting.registerCallback("register_storm_rule", setStormRule);
